a8382484a97c589444e6a9b04af3153ae7ba8491
Shannon Moeller <me@shannonmoeller.com>
2022-11-08 10:54:31 -0700

    fix: account for internal api change (#136)
    Fixes #134.

--- src/checker.js
+++ src/checker.js
@@ -1,41 +1,57 @@
 'use strict';
 
-const stylelintConfigPrettier = require('./index');
-const stylelint = require('stylelint');
 const { resolve } = require('path');
+const stylelint = require('stylelint');
+const stylelintConfigPrettier = require('./index');
+
+function tryRequire(mod) {
+	try {
+		return require(mod);
+	} catch (error) {
+		return null;
+	}
+}
+
+const getConfigForFile = tryRequire('stylelint/lib/getConfigForFile');
 
 const { hasOwnProperty } = Object.prototype;
 
-function check(path) {
+async function getConfig(linter, resolvedPath) {
+	if (linter.getConfigForFile) {
+		return await linter.getConfigForFile(resolvedPath);
+	}
+
+	if (getConfigForFile) {
+		return await getConfigForFile(linter, resolvedPath);
+	}
+
+	throw new Error(`Could not get config for file: ${resolvedPath}`);
+}
+
+async function check(path) {
 	const resolvedPath = resolve(process.cwd(), path || '');
+	const linter = stylelint.createLinter();
+	const config = await getConfig(linter, resolvedPath);
 
-	return stylelint
-		.createLinter()
-		.getConfigForFile(resolvedPath)
-		.then((config) => {
-			const prettierRules = stylelintConfigPrettier.rules;
-			const configRules = config.config.rules;
-			const conflictingRules = [];
+	const prettierRules = stylelintConfigPrettier.rules;
+	const configRules = config.config.rules;
+	const conflictingRules = [];
 
-			function isEmpty(value) {
-				return (
-					value === null ||
-					(Array.isArray(value) && value.length === 0)
-				);
-			}
+	function isEmpty(value) {
+		return value === null || (Array.isArray(value) && value.length === 0);
+	}
 
-			Object.keys(prettierRules).forEach((rule) => {
-				if (
-					hasOwnProperty.call(configRules, rule) &&
-					!isEmpty(configRules[rule]) &&
-					configRules[rule][0] !== prettierRules[rule]
-				) {
-					conflictingRules.push(rule);
-				}
-			});
+	Object.keys(prettierRules).forEach((rule) => {
+		if (
+			hasOwnProperty.call(configRules, rule) &&
+			!isEmpty(configRules[rule]) &&
+			configRules[rule][0] !== prettierRules[rule]
+		) {
+			conflictingRules.push(rule);
+		}
+	});
 
-			return conflictingRules.length ? conflictingRules : null;
-		});
+	return conflictingRules.length ? conflictingRules : null;
 }
 
 exports.check = check;
--- test/checker.js
+++ test/checker.js
@@ -4,16 +4,19 @@ const { check } = require('../src/checker.js');
 async function test() {
 	const error = await check().catch((error) => error);
 
-	assert(/No configuration provided/.test(error.message));
+	assert(
+		/No configuration provided/.test(error.message),
+		'should handle not found'
+	);
 
 	const fail = await check('./test/fail');
 
-	assert(fail.length > 0);
-	assert(fail.includes('indentation'));
+	assert(fail.length > 0, 'should have errors');
+	assert(fail.includes('indentation'), 'should have indent error');
 
 	const pass = await check('./test/pass');
 
-	assert(pass === null);
+	assert(pass == null, 'should have no errors');
 }
 
 test().catch((error) => {
